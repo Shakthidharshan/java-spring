f8884c01c5c44a17de538e6d794077b7dac09dc4
shakthi dharshan <shakthisd223@gmail.com>
2026-01-20 16:39:17 +0530

    CURD operations completed.

--- src/main/java/com/shakthi/spring_boot_practice/controllers/UserController.java
+++ src/main/java/com/shakthi/spring_boot_practice/controllers/UserController.java
@@ -4,11 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jvlcode.spring_boot_demo.exceptions.ResourceNotFoundException;
 import com.shakthi.spring_boot_practice.Entity.bikeUserEntity;
 import com.shakthi.spring_boot_practice.Repository.UserBikeRepo;
 
@@ -28,10 +33,10 @@ public class UserController {
 	
 	@GetMapping("/id/{id}")
 	public bikeUserEntity getUserBikebyId(@PathVariable long id) {
-		return userRepo.findById(id).orElseThrow();
+		return userRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not available with id: " + id));
 	}
 	
-	 @GetMapping("/name/{name}")
+	@GetMapping("/name/{name}")
     public ResponseEntity<bikeUserEntity> getUserBikeByName(@PathVariable String name) {
 		 bikeUserEntity user = userRepo.findByName(name);
 		    if (user == null) {
@@ -39,6 +44,32 @@ public class UserController {
 		    }
 		    return ResponseEntity.ok(user);  
     }
+    
+    @PutMapping("/{id}")
+    public bikeUserEntity updateuserdetails(@PathVariable long id , @RequestBody bikeUserEntity users ) {
+		    bikeUserEntity userdata = userRepo.findById(id).orElseThrow();
+		    userdata.setName(users.getName());
+		    userdata.setAddress(users.getAddress());
+		    userdata.setEngineType(users.getEngineType());
+		    userdata.setPrice(users.getPrice());
+		    userdata.setType(users.getType());
+		    userdata.setCc(users.getCc());
+		    return userRepo.save(userdata);
+    }
+    
+    @PostMapping
+    public bikeUserEntity createUserDetails(@RequestBody bikeUserEntity users) {
+    	return userRepo.save(users);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUserDetails(@PathVariable long id) {
+    	bikeUserEntity userdata = userRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not available with id: " + id));
+    	userRepo.delete(userdata);
+    	return ResponseEntity.ok().build();
+    }
+    
+   
 	
 	
 }
